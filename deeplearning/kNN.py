ab9b86027c7c45882f8aa939a6ab88499073d503
梅海磊 <meihailei@meihl.local>
2020-06-23 12:31:36 +0800

    测试

--- deeplearning/kNN.py
+++ deeplearning/kNN.py
@@ -1,45 +1,47 @@
-from numpy import *
-import operator
+
 
 
 def createDataSet():
-    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
-
+import numpy as np
+import operator
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
-    # 距离计算
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat ** 2
+    #❶（以下三行）距离计算
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances ** 0.5
-    sortedDisIndicies = distances.argsort()
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
     classCount = {}
-    # 选择距离最小的k个点
+    #❷ （以下两行）选择距离最小的k个点
     for i in range(k):
-        voteIlable = labels[sortedDisIndicies[i]]
-        classCount[voteIlable] = classCount.get(voteIlable, 0) + 1
-        # 排序
-        sortedClassCount = sorted(classCount.items(),
-                                  key=operator.itemgetter(1), reverse=True)
-        return sortedClassCount[0][0]
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(
+        classCount.items(),
+        #❸ 排序
+        key=operator.itemgetter(1),
+        reverse=True)
+    return sortedClassCount[0][0]
 
 
 def file2matrix(filename):
     love_dictionary = {'largeDoses': 3, 'smallDoses': 2, 'didntLike': 1}
     fr = open(filename)
     arrayOLines = fr.readlines()
-    numberOfLines = len(arrayOLines)  # get the number of lines in the file
-    returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
-    classLabelVector = []  # prepare labels return
+    numberOfLines = len(arrayOLines)  #get the number of lines in the file
+    returnMat = np.zeros((numberOfLines, 3))  #prepare matrix to return
+    classLabelVector = []  #prepare labels return
     index = 0
     for line in arrayOLines:
         line = line.strip()
         listFromLine = line.split('\t')
         returnMat[index, :] = listFromLine[0:3]
-        if listFromLine[-1].isdigit():
+        if (listFromLine[-1].isdigit()):
             classLabelVector.append(int(listFromLine[-1]))
         else:
             classLabelVector.append(love_dictionary.get(listFromLine[-1]))
@@ -51,23 +53,28 @@ def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
+    normDataSet = np.zeros(np.shape(dataSet))
     m = dataSet.shape[0]
-    normDataSet = dataSet = tile(minVals, (m, 1))
-    normDataSet = normDataSet / tile(ranges, (m, 1))
+    normDataSet = dataSet - np.tile(minVals, (m, 1))
+    normDataSet = normDataSet / np.tile(ranges, (m, 1))
     return normDataSet, ranges, minVals
 
+
 def datingClassTest():
-    hoRatio = 0.10
-    datingDataMat,datingLabels = file2matrix('datingTestSet.txt')
+    hoRatio = 0.50
+    # 处理数据
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    # 归一化处理
     normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0] 
-    numTestVecs = int(m*hoRatio) 
+    m = normMat.shape[0]
+    # 获取用于测试的数据量
+    numTestVecs = int(m * hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],\
-                     datingLabels[numTestVecs:m],3)
-        print "the classifier came back with: %d, the real answer is: %d"\
-                     % (classifierResult, datingLabels[i])
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" %
+              (classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+    print(errorCount)
